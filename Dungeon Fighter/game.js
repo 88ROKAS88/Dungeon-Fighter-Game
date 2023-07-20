89c3115051ecd33b0bd7ebf69da43695fd73540f
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2023-07-20 22:59:48 +0300

    canvas
    draw player on canvas

--- Dungeon Fighter/game.js
+++ Dungeon Fighter/game.js
@@ -1,5 +1,46 @@
-const app = document.querySelector("#app");
+// canvas
+const canvas = document.getElementById("canvas");
+// test.innerText = "test";
+// canvas.style.width = "100%";
+// canvas.style.height = "500px";
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
 
-const test = document.createElement("h1");
-test.innerText = "test";
-app.appendChild(test);
+// ctx
+const ctx = canvas.getContext("2d");
+
+// player
+let playerPositionX = 0;
+let playerPositionY = 0;
+function drawPlayer() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(playerPositionY, playerPositionX, 100, 100);
+}
+
+// animate
+function animate() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  drawPlayer();
+  requestAnimationFrame(animate);
+}
+animate();
+
+// keyboart listener
+window.addEventListener("keydown", (e) => {
+  if (e.key == "ArrowUp") {
+    playerPositionX--;
+  }
+  if (e.key == "ArrowDown") {
+    playerPositionX++;
+  }
+  if (e.key == "ArrowLeft") {
+    playerPositionY--;
+  }
+  if (e.key == "ArrowRight") {
+    playerPositionY++;
+  }
+  console.log(e.key);
+});
+// test
+console.log(canvasWidth);
+console.log(canvasHeight);
