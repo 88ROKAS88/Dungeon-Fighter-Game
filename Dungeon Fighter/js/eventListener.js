f5cdb5e4b4fd2c1946411bb4639ed566621c1f13
88ROKAS88 <rokas8misevicius@gmail.com>
2023-11-17 19:54:49 +0200

    Update eventListener
    bug fix. (after releasing any button stops all movements).

--- Dungeon Fighter/js/eventListener.js
+++ Dungeon Fighter/js/eventListener.js
@@ -14,29 +14,19 @@ window.addEventListener("keydown", (e) => {
     game.player.changeY = 2;
     game.player.row = 0;
   }
-  // console.log("keydown", e.key);
 });
 
 window.addEventListener("keyup", (e) => {
-  if (
-    e.key == "ArrowUp" ||
-    e.key == "ArrowDown" ||
-    e.key == "ArrowLeft" ||
-    e.key == "ArrowRight"
-  ) {
-    game.player.changeY = 0;
-    game.player.changeX = 0;
+  if (e.key == "ArrowUp") {
+    game.player.changeX += 2;
   }
-  // if (e.key == "ArrowDown") {
-  //   game.player.positionX++;
-  // }
-  // if (e.key == "ArrowLeft") {
-  //   game.player.positionY--;
-  //   game.player.row = 200;
-  // }
-  // if (e.key == "ArrowRight") {
-  //   game.player.positionY++;
-  //   game.player.row = 0;
-  // }
-  // console.log("keyup", e.key);
+  if (e.key == "ArrowDown") {
+    game.player.changeX += -2;
+  }
+  if (e.key == "ArrowLeft") {
+    game.player.changeY += 2;
+  }
+  if (e.key == "ArrowRight") {
+    game.player.changeY += -2;
+  }
 });
